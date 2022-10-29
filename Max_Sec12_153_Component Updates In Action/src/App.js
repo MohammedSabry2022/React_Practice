c35e9d783f8befe2cdf570cc2a8f288d04e67bb6
mohammedsabry2022 <mohammedsabry2022@hotmail.com>
2022-10-29 15:53:27 +0200

    callback

--- Max_Sec12_153_Component Updates In Action/src/App.js
+++ Max_Sec12_153_Component Updates In Action/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "../src/components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleParagrapHandler = () => {
+
+  console.log("App Running");
+
+  const toggleParagrapHandler = useCallback(() => {
     setShowParagraph((preShowParagraph) => !preShowParagraph);
-  };
+  }, []);
+
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagrapHandler}>Toggle Paragraph!</Button>
     </div>
   );
