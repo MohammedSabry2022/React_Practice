8e9aa839d09723f0e31acae17fed4312a454ef04
mohammedsabry2022 <mohammedsabry2022@hotmail.com>
2022-11-01 13:34:25 +0200

    memo optimization

--- Max_Sec12_153_Component Updates In Action/src/App.js
+++ Max_Sec12_153_Component Updates In Action/src/App.js
@@ -5,17 +5,25 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("App Running");
 
   const toggleParagrapHandler = useCallback(() => {
-    setShowParagraph((preShowParagraph) => !preShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((preShowParagraph) => !preShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagrapHandler}>Toggle Paragraph!</Button>
     </div>
   );
