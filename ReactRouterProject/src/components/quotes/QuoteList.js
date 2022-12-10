adf7af40166f542849593616677c214d396fca16
mohammedsabry2022 <mohammedsabry2022@hotmail.com>
2022-12-10 12:47:19 +0200

    Writing More Flexible Routing Code

--- ReactRouterProject/src/components/quotes/QuoteList.js
+++ ReactRouterProject/src/components/quotes/QuoteList.js
@@ -16,12 +16,16 @@ const sortQuotes = (quotes, ascending) => {
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
+  console.log(location);
   const queryParams = new URLSearchParams(location.search);
   const isSortingAscending = queryParams.get("sort") === "asc";
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const sortChangeHandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`,
+    });
   };
   return (
     <Fragment>
--- ReactRouterProject/src/pages/QuoteDetail.js
+++ ReactRouterProject/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const QuoteDetail = () => {
@@ -8,6 +8,7 @@ const QuoteDetail = () => {
     { id: "q2", author: "Maximilian", text: "Learning React is great!" },
   ];
 
+  const match = useRouteMatch();
   const params = useParams();
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -18,7 +19,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
