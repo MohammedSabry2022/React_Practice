e2203513166104591054f89ba073cb64cab06c0e
mohammedsabry2022 <mohammedsabry2022@hotmail.com>
2022-12-12 13:11:50 +0200

    Finished

--- ReactRouterProject/src/components/comments/NewCommentForm.js
+++ ReactRouterProject/src/components/comments/NewCommentForm.js
@@ -1,9 +1,11 @@
-import { useRef } from 'react';
-
-import classes from './NewCommentForm.module.css';
-
+import { useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import classes from "./NewCommentForm.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner";
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, data } = useHttp(addComment);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -11,10 +13,17 @@ const NewCommentForm = (props) => {
     // optional: Could validate here
 
     // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentData: enteredText });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className='centerd'>
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
